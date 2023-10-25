ed157579b8fa21c1fbe6600034579778cc766c80
Ameer <ameerapex496@gmail.com>
2023-10-26 01:32:49 +0200

    finished navbar functions

---
+++ src/navbar.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.getElementsByClassName("togglebtn")[0];
+const menuBtnImg = document.getElementsByClassName("menu-btn-img")[0];
+const navLinks = document.getElementsByClassName("navlinks")[0];
+const navBrand = document.getElementsByClassName("navbrand")[0];
+const body = document.querySelector("body");
+const navBar = document.getElementsByClassName("navbar")[0];
+
+toggleBtn.addEventListener("click", () => {
+  if (!navLinks.classList.contains("active")) {
+    menuBtnImg.src = "./assets/closem.png";
+    navBar.classList.add("active");
+    navLinks.classList.add("active");
+    body.classList.add("active");
+    navBrand.style.display = "none";
+  } else {
+    navLinks.classList.remove("active");
+    menuBtnImg.src = "./assets/openm.png";
+    body.classList.remove("active");
+    navBar.classList.remove("active");
+    navBrand.style.display = "block";
+  }
+});
